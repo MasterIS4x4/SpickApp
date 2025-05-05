a06e07e1adbe31012d8578bcadbeef1e5c475f14
voidlord <lehelkov35@gmail.com>
2025-05-05 19:09:47 +0300

    Disable react strict mode as it usually causes more issues than be helpful

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
@@ -7,9 +6,7 @@ import { store } from './store'
 const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 )

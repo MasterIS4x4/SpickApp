1c83c3af306a2cac938feb5c5c0230d4e70618a6
Jindřich Bobek <bobekji1@uhk.cz>
2025-05-13 11:35:28 +0300

    Change LessonsPage style

--- src/pages/LessonsPage.tsx
+++ src/pages/LessonsPage.tsx
@@ -1,11 +1,23 @@
-import { IonContent, IonIcon, IonItem, IonLabel, IonList } from '@ionic/react'
+import {
+  IonBadge,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonIcon,
+  IonRow,
+} from '@ionic/react'
 import { useAppDispatch, useAppSelector } from '../store'
 import { useEffect, useState } from 'react'
 import { setCurrentTab } from '../reducers/navigation'
 import { ILesson } from '../model/lesson'
 import { getLessons } from '../service/lesson'
 import { BilingualTitle } from '../components/BilingualTitle'
-import { book } from 'ionicons/icons'
+import { book, checkmarkCircleOutline } from 'ionicons/icons'
 import { basePath } from '../App'
 
 export const LessonsPage = () => {
@@ -24,25 +36,70 @@ export const LessonsPage = () => {
       })
   }, [])
 
+  const isLessonCompleted = (lesson: ILesson) => {
+    return lesson.id === '1' // TODO: replace with actual logic
+  }
+
   // TODO: mark current lesson
   // - display number of quizes done per lesson
   // add icon per lesson
 
   return (
-    <IonContent>
+    <IonContent className="ion-padding">
       <BilingualTitle ro={'Lecții'} eng={'Lessons'} />
-      <IonList>
-        {lessons.map((lesson, index) => (
-          <IonItem key={index} routerLink={basePath + `lessons/${lesson.id}`}>
-            <IonIcon icon={book} slot="start" />
-            <IonLabel>
-              <h2>
-                {index + 1}. {lesson.nameRO} - {lesson.nameEN}
-              </h2>
-            </IonLabel>
-          </IonItem>
-        ))}
-      </IonList>
+      <IonGrid>
+        <IonRow>
+          {lessons.map((lesson, index) => {
+            const isCompleted = isLessonCompleted(lesson)
+            return (
+              <IonCol key={lesson.id} size="12" sizeMd="6" sizeLg="4">
+                <IonCard
+                  button
+                  routerLink={basePath + `lessons/${lesson.id}`}
+                  style={{
+                    textAlign: 'center',
+                    padding: '1em',
+                    border: isCompleted ? '2px solid var(--ion-color-success)' : undefined,
+                    opacity: isCompleted ? 1 : 0.95,
+                    position: 'relative',
+                  }}
+                >
+                  {isCompleted && (
+                    <IonIcon
+                      icon={checkmarkCircleOutline}
+                      color="success"
+                      style={{
+                        position: 'absolute',
+                        top: '0.5rem',
+                        right: '0.5rem',
+                        fontSize: '1.8rem',
+                      }}
+                    />
+                  )}
+
+                  <IonCardHeader>
+                    <div style={{ fontSize: '2rem' }}>
+                      <IonIcon icon={book} />
+                      {/* TODO: Replace with a custom icon per lesson */}
+                    </div>
+                    <IonCardTitle>
+                      {index + 1}. {lesson.nameRO}
+                    </IonCardTitle>
+                    <IonCardSubtitle>{lesson.nameEN}</IonCardSubtitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    {isCompleted && (
+                      <IonBadge color="success" style={{ marginTop: '0.5rem' }}>
+                        Lesson completed
+                      </IonBadge>
+                    )}
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            )
+          })}
+        </IonRow>
+      </IonGrid>
     </IonContent>
   )
 }
